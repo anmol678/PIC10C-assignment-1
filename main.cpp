5f1185fef71d75c50f9e0bff2f14ce4744db2aee
anmol678 <anmolj678@gmail.com>
2016-10-21 15:41:07 -0700

    Updated main: Player can no more bet more than total money

--- main.cpp
+++ main.cpp
@@ -35,88 +35,96 @@ int main() {
         << "Enter bet: ";
         cin >> bet; //The bet is input by the player
         
-        do {
-            cout <<"\nYour cards:\n";
-            handPlayer.print(); //The player's cards are displayed
+        //If the bet exceeds total money, the player is asked to re-enter the bet
+        if (bet > money) {
+            cout << "You don't have that much money. Please bet again." << endl;
+        }
+        
+        else {
             
-            cout << "Your total is " << handPlayer.sum() << ". Do you want another card (y/n)? ";
-            cin >> choice; //Total value of cards is displayed and the player is given a choice to draw another card
-            
-            //If the player declines to draw another card the dealer's cards are displayed and dealer takes the game forward. Subsequently, the total values of player's hand and dealer's hand are compared and it is decided if the player wins or loses the bet.
-            if (choice == 'n') {
-                cout << "\nDealer's cards: \n";
-                handDealer.print();
-                cout << "The dealer's total is " << handDealer.sum() << ".\n";
+            do {
+                cout <<"\nYour cards:\n";
+                handPlayer.print(); //The player's cards are displayed
                 
-                while (handDealer.sum() < 5.5){//The dealer continues to draw cards till the total value <= 5.5
-                    handDealer.drawCard();
-                    cout << "\nNew card:\n";
-                    handDealer.printLast();//The new card drawn is displayed
-                    
+                cout << "Your total is " << handPlayer.sum() << ". Do you want another card (y/n)? ";
+                cin >> choice; //Total value of cards is displayed and the player is given a choice to draw another card
+                
+                //If the player declines to draw another card the dealer's cards are displayed and dealer takes the game forward. Subsequently, the total values of player's hand and dealer's hand are compared and it is decided if the player wins or loses the bet.
+                if (choice == 'n') {
                     cout << "\nDealer's cards: \n";
                     handDealer.print();
                     cout << "The dealer's total is " << handDealer.sum() << ".\n";
-                }
-                
-                //The total values of the dealer's hand and the player's hand are compared
-                //The player loses the bet if he busts
-                if (handPlayer.sum() > 7.5) {
-                    cout << "\nToo bad. You lose " << bet << ".\n";
-                    player.updateMoney(-bet);
-                }
-                
-                //The player wins the bet if he doesn't bust but the dealer busts
-                else if (handPlayer.sum() <= 7.5 && handDealer.sum() > 7.5) {
-                    cout << "\nYou win " << bet << ".\n";
-                    player.updateMoney(bet);
-                }
-                
-                //It's a tie if both the player and the dealer don't bust and the values are equal
-                else if (handPlayer.sum() == handDealer.sum()) {
-                    cout << "\nIts a tie.\n";
-                }
-                
-                //If both the player and the dealer do not bust:
-                else {
                     
-                    //The player wins the bet if he closer to 7.5 than the dealer is
-                    if (handDealer < handPlayer) {
+                    while (handDealer.sum() < 5.5){//The dealer continues to draw cards till the total value <= 5.5
+                        handDealer.drawCard();
+                        cout << "\nNew card:\n";
+                        handDealer.printLast();//The new card drawn is displayed
+                        
+                        cout << "\nDealer's cards: \n";
+                        handDealer.print();
+                        cout << "The dealer's total is " << handDealer.sum() << ".\n";
+                    }
+                    
+                    //The total values of the dealer's hand and the player's hand are compared
+                    //The player loses the bet if he busts
+                    if (handPlayer.sum() > 7.5) {
+                        cout << "\nToo bad. You lose " << bet << ".\n";
+                        player.updateMoney(-bet);
+                    }
+                    
+                    //The player wins the bet if he doesn't bust but the dealer busts
+                    else if (handPlayer.sum() <= 7.5 && handDealer.sum() > 7.5) {
                         cout << "\nYou win " << bet << ".\n";
                         player.updateMoney(bet);
                     }
                     
-                    //The player loses the bet if the dealer is closer to 7.5 than he is
+                    //It's a tie if both the player and the dealer don't bust and the values are equal
+                    else if (handPlayer.sum() == handDealer.sum()) {
+                        cout << "\nIts a tie.\n";
+                    }
+                    
+                    //If both the player and the dealer do not bust:
                     else {
-                        cout << "\nToo bad. You lose " << bet << ".\n";
-                        player.updateMoney(-bet);
+                        
+                        //The player wins the bet if he closer to 7.5 than the dealer is
+                        if (handDealer < handPlayer) {
+                            cout << "\nYou win " << bet << ".\n";
+                            player.updateMoney(bet);
+                        }
+                        
+                        //The player loses the bet if the dealer is closer to 7.5 than he is
+                        else {
+                            cout << "\nToo bad. You lose " << bet << ".\n";
+                            player.updateMoney(-bet);
+                        }
                     }
                 }
+                
+                //If the player chooses to draw another card:
+                else {
+                    handPlayer.drawCard();
+                    cout << "\nNew card:\n";
+                    handPlayer.printLast();
+                }
+            } while (choice == 'y');//This loop iterates only if the player opts to draw another card. Otherwise, since the player's turn is over once the dealer draws his cards and values of the two hands are compared the game ends.
+            //The player cannot play anymore if he runs out of money
+            if (player.getMoney() == 0) {
+                cout << "\nYou have $0. GAME OVER!\nCome back when you have more money.\n\nBye!";
+                
+                break;
             }
             
-            //If the player chooses to draw another card:
+            //The player cannot play anymore if the dealer looses 900 or more
+            else if (player.getMoney() >= 1000) {
+                cout << "\n"Congratulations. You beat the casino!\n\nBye!";
+                
+                break;
+            }
+            
+            //If none of the above scenarios apply a new game begins
             else {
-                handPlayer.drawCard();
-                cout << "\nNew card:\n";
-                handPlayer.printLast();
+                continue;
             }
-        } while (choice == 'y');//This loop iterates only if the player opts to draw another card. Otherwise, since the player's turn is over once the dealer draws his cards and values of the two hands are compared the game ends.
-        //The player cannot play anymore if he runs out of money
-        if (player.getMoney() == 0) {
-            cout << "\nYou have $0. GAME OVER!\nCome back when you have more money.\n\nBye!";
-            
-            break;
-        }
-        
-        //The player cannot play anymore if the dealer looses 900 or more
-        else if (player.getMoney() >= 1000) {
-            cout << "\n"Congratulations. You beat the casino!\n\nBye!";
-            
-            break;
-        }
-        
-        //If none of the above scenarios apply a new game begins
-        else {
-            continue;
         }
     } while (player.getMoney() > 0 && player.getMoney() < 1000); //This loop reiterates while the player hasn't lost all his money and the dealer hasn't lost 900 or more. Upon reiteration, new hands for both the player and the dealer are intialized and a new game begins.
     
